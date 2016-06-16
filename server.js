017a1ca86ef1ea8a09b4e1d59364833b40d983dc
Nick Lewis <nick@headforcode.com>
2016-06-16 15:53:43 +0100

    Fixed port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from 'express';
 import { apolloServer } from 'graphql-tools';
 import Schema from './data/schema';
 
-const GRAPHQL_PORT = 8080;
+const GRAPHQL_PORT = process.env.PORT || 8080;;
 
 const graphQLServer = express();
 
